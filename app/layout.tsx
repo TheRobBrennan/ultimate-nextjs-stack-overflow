fd55f235d5d2de0fb3119147d8b007ec774342e6
Rob Brennan <rob@therobbrennan.com>
2023-11-04 12:00:20 -0700

    Add initial styling and metadata to global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,30 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+
+// Use our global styles
+import "./globals.css";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+// Metadata for the site
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A community-driven platform for asking and answering questions",
+  icons: {
+    icon: "/assets/images/site-log.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +34,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
