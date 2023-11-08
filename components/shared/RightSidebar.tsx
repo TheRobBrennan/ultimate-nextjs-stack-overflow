0229b2cac65133007480af4327e9631b03c7bd38
Rob Brennan <rob@therobbrennan.com>
2023-11-08 15:52:51 -0800

    Add right sidebar and render tag components

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import RenderTag from "./RenderTag";
 
 // TODO: Mock data - this will be replaced with data from the database
 const hotQuestions = [
@@ -13,6 +14,15 @@ const hotQuestions = [
       "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
   },
   { _id: 5, title: "Redux Toolkit Not Updating State as Expected" },
+];
+
+// TODO: Mock data - this will be replaced with data from the database
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 5 },
+  { _id: 3, name: "next", totalQuestions: 5 },
+  { _id: 4, name: "vue", totalQuestions: 2 },
+  { _id: 5, name: "redux", totalQuestions: 10 },
 ];
 
 const RightSidebar = () => {
@@ -41,7 +51,20 @@ const RightSidebar = () => {
           ))}
         </div>
       </div>
-      <div className="mt-16">test2</div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
