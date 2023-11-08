a0c9f2b07615b4270d82ca820fe56167521a364c
Rob Brennan <rob@therobbrennan.com>
2023-11-08 14:50:23 -0800

    Add login and sign up buttons to the left sidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,6 +5,8 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
@@ -41,6 +43,24 @@ const LeftSidebar = () => {
           );
         })}
       </div>
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <span className="primary-text-gradient">Log In</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
     </section>
   );
 };
