56c726405cdcb8307cffed68814a611aef20a8ae
Rob Brennan <rob@therobbrennan.com>
2023-11-08 15:02:37 -0800

    Improve styling so that the log in and sign up buttons display icons instead of text at the appropriate breakpoint

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -36,7 +36,11 @@ const LeftSidebar = () => {
                 height={20}
                 className={`${isActive ? "" : "invert-colors"}`}
               />
-              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
+              >
                 {link.label}
               </p>
             </Link>
@@ -47,7 +51,16 @@ const LeftSidebar = () => {
         <div className="flex flex-col gap-3">
           <Link href="/sign-in">
             <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              <span className="primary-text-gradient">Log In</span>
+              <Image
+                src={"/assets/icons/account.svg"}
+                alt="Log In"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
             </Button>
           </Link>
         </div>
@@ -56,7 +69,14 @@ const LeftSidebar = () => {
         <div className="flex flex-col gap-3">
           <Link href="/sign-up">
             <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              Sign Up
+              <Image
+                src={"/assets/icons/sign-up.svg"}
+                alt="Sign Up"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign Up</span>
             </Button>
           </Link>
         </div>
